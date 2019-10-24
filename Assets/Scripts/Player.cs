92dd7b85a1220a35b575a7eb3bc63f6a3b185180
Daniel Blázquez Juan <dani.b.j@hotmail.com>
2019-10-24 18:19:19 +0200

    Created a Test Scene and started to add the main movements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 10;
+    private float jumpSpeed = 5;
+    float playerHeight;
+
+    Vector3 startPosition;
     void Start()
     {
-        
+        startPosition = transform.position;
+        playerHeight = GetComponent<Collider2D>().bounds.size.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit2D hit =
+                       Physics2D.Raycast(
+                           transform.position,
+                           new Vector2(0, -1));
+
+            float floorDistance = hit.distance;
+            bool touchingGround = floorDistance < playerHeight * 0.6f;
+            if (touchingGround)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+            }
+        }
     }
 }
